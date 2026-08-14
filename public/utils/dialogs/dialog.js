6be2b52f1030cc103871cc36ed1e07b9ef58ead4
Brandon <84153483+Salypse@users.noreply.github.com>
2026-08-14 14:24:47 -0500

    Fix dialog state on browser back button

--- public/utils/dialogs/dialog.js
+++ public/utils/dialogs/dialog.js
@@ -1,6 +1,12 @@
 const openModalBtns = document.querySelectorAll(".open-modal");
 const closeModalBtns = document.querySelectorAll(".close-modal");
 
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
 openModalBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     document.getElementById(btn.dataset.target).showModal();
