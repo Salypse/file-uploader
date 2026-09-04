5338ddd59da25c667a5f4bd6eafca5648673e820
Brandon <84153483+Salypse@users.noreply.github.com>
2026-09-04 13:32:47 -0500

    Update index route to redirect not logged in users to /login

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,19 +2,23 @@ const { prisma } = require("../lib/prisma");
 const { getContent } = require("../public/utils/fileBrowserUtils");
 
 exports.indexGet = async (req, res, next) => {
-  try {
-    let content = [];
-    const flashErrors = req.flash("error");
+  if (req.user) {
+    try {
+      let content = [];
+      const flashErrors = req.flash("error");
 
-    if (req.user) {
-      content = await getContent(null, req.user.id);
+      if (req.user) {
+        content = await getContent(null, req.user.id);
+      }
+
+      return res.render("folder", {
+        content: content,
+        errors: flashErrors,
+      });
+    } catch (error) {
+      return next(error);
     }
-
-    res.render("folder", {
-      content: content,
-      errors: flashErrors,
-    });
-  } catch (error) {
-    return next(error);
   }
+
+  res.redirect("/login");
 };
