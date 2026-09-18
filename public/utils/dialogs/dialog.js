f34695f9a98cfab5f6401b92f1bc37069c6af302
Brandon <84153483+Salypse@users.noreply.github.com>
2026-09-18 13:26:42 -0500

    Fix modal action double input causing error

--- public/utils/dialogs/dialog.js
+++ public/utils/dialogs/dialog.js
@@ -1,6 +1,21 @@
 const openModalBtns = document.querySelectorAll(".open-modal");
 const closeModalBtns = document.querySelectorAll(".close-modal");
 
+const modals = document.querySelectorAll(".modal");
+
+// Stop Button presses while action is loading/completing
+modals.forEach((modal) => {
+  const closeBtn = modal.querySelector(".close-modal");
+  const actionBtn = modal.querySelector(".modal-action-btn");
+
+  const form = modal.querySelector("form");
+
+  form.addEventListener("submit", () => {
+    actionBtn.disabled = true;
+    closeBtn.disabled = true;
+  });
+});
+
 window.addEventListener("pageshow", (event) => {
   const navigation = performance.getEntriesByType("navigation")[0];
 
